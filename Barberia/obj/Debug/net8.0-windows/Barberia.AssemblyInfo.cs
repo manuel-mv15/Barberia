dc6a0809a321a94a3b7c93615ea044a71797affd
Manuel-MV15 <mmelendez1505@gmail.com>
2024-06-27 22:56:56 -0600

    Buscar por nombre en el home

--- Barberia/obj/Debug/net8.0-windows/Barberia.AssemblyInfo.cs
+++ Barberia/obj/Debug/net8.0-windows/Barberia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barberia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fd52a0cb52dc86dda7915204c1772b18e9768c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58336ad134e603c96457eefb6ba80b229b3a5271")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barberia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barberia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
